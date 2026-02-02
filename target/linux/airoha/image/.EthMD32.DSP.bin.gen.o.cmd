94fb501bbda153dc9669e587e663f03f08b2ba21
brudalevante <170347513+brudalevante@users.noreply.github.com>
2026-02-02 23:56:58 +0100

    Add files via upload

---
+++ target/linux/airoha/image/.EthMD32.DSP.bin.gen.o.cmd
@@ -0,0 +1,15 @@
+savedcmd_drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o := aarch64-openwrt-linux-musl-gcc -Wp,-MMD,drivers/base/firmware_loader/builtin/airoha/.EthMD32.DSP.bin.gen.o.d -nostdinc -I./arch/arm64/include -I./arch/arm64/include/generated  -I./include -I./arch/arm64/include/uapi -I./arch/arm64/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/compiler-version.h -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -DKASAN_SHADOW_SCALE_SHIFT= -fmacro-prefix-map=./= -Werror -D__ASSEMBLY__ -fno-PIE -mabi=lp64 -fno-asynchronous-unwind-tables -fno-unwind-tables -DKASAN_SHADOW_SCALE_SHIFT= -g    -c -o drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.S 
+
+source_drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o := drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.S
+
+deps_drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o := \
+  include/linux/compiler-version.h \
+    $(wildcard include/config/CC_VERSION_TEXT) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/CPU_BIG_ENDIAN) \
+    $(wildcard include/config/BOOGER) \
+    $(wildcard include/config/FOO) \
+
+drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o: $(deps_drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o)
+
+$(deps_drivers/base/firmware_loader/builtin/airoha/EthMD32.DSP.bin.gen.o):
